8b18f8bbdf197fecafb4bac34362f5a4c5d19cf2
Sam Stanford <samstanford99@gmail.com>
2024-01-29 21:12:00 +0000

    style(footer): Remove unused code

--- src/pages/HomePage/sections/FooterSection.tsx
+++ src/pages/HomePage/sections/FooterSection.tsx
@@ -6,7 +6,7 @@ import Section, {
 } from "../../../components/Section/Section";
 import orangeArrow from "../../../assets/images/icons/arrow_right_orange.svg";
 import ChangingWord from "../../../components/ChaningWord";
-import { useCallback, useRef } from "react";
+import { useRef } from "react";
 
 const BLACK = "#262626";
 
@@ -43,42 +43,8 @@ const ADJECTIVES = [
   "talented",
 ];
 
-const MAX_OFFSET = 200;
-
 export default function FooterSection() {
   const mainTextRef = useRef<HTMLDivElement>(null);
-
-  const getDocHeight = () => {
-    return Math.max(
-      document.body.scrollHeight,
-      document.documentElement.scrollHeight,
-      document.body.offsetHeight,
-      document.documentElement.offsetHeight,
-      document.body.clientHeight,
-      document.documentElement.clientHeight
-    );
-  };
-
-  const updatePosition = useCallback(() => {
-    const scrollTop = window.pageYOffset;
-    const windowHeight = window.innerHeight;
-    const docHeight = getDocHeight();
-
-    const footerPageRatio = windowHeight / docHeight;
-
-    const totalDocScrollLength = docHeight - windowHeight;
-    const scrollPostion = scrollTop / totalDocScrollLength;
-
-    const percentageOfFooterScrolled =
-      (scrollPostion - (1 - footerPageRatio)) / footerPageRatio;
-
-    let newOffset = MAX_OFFSET * (1 - percentageOfFooterScrolled) * 0.3;
-    newOffset = newOffset > MAX_OFFSET ? MAX_OFFSET : newOffset;
-
-    if (mainTextRef.current) {
-      mainTextRef.current.style.transform = `translate3d(0,${-newOffset}%,0)`;
-    }
-  }, []);
 
   return (
     <>
@@ -92,13 +58,11 @@ export default function FooterSection() {
             ref={mainTextRef}
             style={{
               position: "relative",
-              // top: "-100px",
               display: "flex",
               flexDirection: "column",
               alignItems: "center",
               justifyContent: "center",
               width: "100%",
-              //   transform: `translate3d(0,0,0)`,
             }}
           >
             <Title
@@ -117,7 +81,7 @@ export default function FooterSection() {
               engineer?
             </Title>
             <PillButton
-              href={"https://www.linkedin.com/in/sam-stanford/"} /* TODO */
+              href={"https://www.linkedin.com/in/sam-stanford/"}
               text="Let's Connect"
               textColor={BLACK}
               backgroundColor="white"
@@ -125,39 +89,6 @@ export default function FooterSection() {
               targetNewTab
             />
           </div>
-
-          {/* <div
-            style={{
-              position: "absolute",
-              bottom: "2em",
-              display: "flex",
-              alignItems: "flex-start",
-              justifyContent: "space-between",
-              width: "100%",
-            }}
-          >
-            <div style={{ display: "flex", alignItems: "center" }}>
-              <a
-                href="https://www.linkedin.com/in/sam-stanford/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <Title level={5} underline style={{ marginRight: "20px" }}>
-                  LinkedIn
-                </Title>
-              </a>
-              <a href="/Sam_Stanford_CV.pdf" target="_blank" rel="noreferrer">
-                <Title level={5} underline>
-                  CV
-                </Title>
-              </a>
-            </div>
-            {/* <Popover trigger="hover">
-              <Title level={5} underline style={{ cursor: "pointer" }}>
-                Designed &amp; Developed By Me
-              </Title>
-            </Popover>
-          </div> */}
         </Content>
       </Section>
     </>
